a583ae99f4dc14dde367c1c86195d8f7c754b084
Minkyu SHIM <hello.mkshim@gmail.com>
2026-03-19 13:28:15 +0100

    feat(cli-scenarios): scenario 2 and 3:

--- index.ts
+++ index.ts
@@ -17,7 +17,7 @@ emitter.subscribe("EnrollmentCancelled", (e) => console.log("Enrollemnt has been
 // construct things that will be used in cli scenarios
 const studentId01 = createStudentId("STU000001")
 const studentEmail01 = createEmail("armand@school.com")
-const studentCredit01 = createCredits(3)
+const studentCredit01 = createCredits(1)
 if (studentId01 instanceof Error) throw studentId01;
 if (studentEmail01 instanceof Error) throw studentEmail01
 if (studentCredit01 instanceof Error) throw studentCredit01;
@@ -35,8 +35,25 @@ const credits3 = createCredits(3)
 if (credits3 instanceof Error) throw credits3
 const pythonCourse = new Course(pythonCourseCode, "Introduction to Python", credits3, 40, 0)
 
-const enrollments: Enrollment[] = []
+const enrollmentsPythonCourse: Enrollment[] = []
 
-const scenario1 = service.enroll(student01, pythonCourse, fall2025, enrollments)
-if(!(scenario1 instanceof Error)) enrollments.push(scenario1)
+const scenario1 = service.enroll(student01, pythonCourse, fall2025, enrollmentsPythonCourse)
+if(!(scenario1 instanceof Error)) enrollmentsPythonCourse.push(scenario1)
 
+// Scenario 2 - Course reaches 80% capacity
+const jsCourseCode = createCourseCode("JS101")
+if (jsCourseCode instanceof Error) throw jsCourseCode
+const enrollmentsJsCourse: Enrollment[] = []
+const jsCourse = new Course(jsCourseCode, "Advanced Web Development", credits3, 5, 3) // it currently start from 3/5 enrollemtents
+const scenario2 = service.enroll(student01, jsCourse, fall2025, enrollmentsJsCourse) // should notify that 80% of the capacity reached
+if(!(scenario2 instanceof Error)) enrollmentsJsCourse.push(scenario2)
+
+// Scenario 3 - Course becomes full
+const studentId02 = createStudentId("STU000002")
+if (studentId02 instanceof Error) throw studentId02
+const studentEmail02 = createEmail("minkyu@school.com")
+if (studentEmail02 instanceof Error) throw studentEmail02
+const student02 = new Student(studentId02, "Minkyu", studentEmail02, studentCredit01)
+
+const scenario3 = service.enroll(student02, jsCourse, fall2025, enrollmentsJsCourse) // should log that the js course is full
+if(!(scenario3 instanceof Error)) enrollmentsJsCourse.push(scenario3)
